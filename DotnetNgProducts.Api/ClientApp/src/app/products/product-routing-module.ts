82c1c2724039e33f0457334b4540473a3812fc0a
Luis Henrique Paim <luis.paim@poatek.com>
2021-05-30 20:20:44 -0300

    Fix main link url and auth guards in children routes

--- DotnetNgProducts.Api/ClientApp/src/app/products/product-routing-module.ts
+++ DotnetNgProducts.Api/ClientApp/src/app/products/product-routing-module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core'
 import { Routes, RouterModule } from '@angular/router'
+import { AuthGuard } from '../guards/auth.guard'
 
 import { ProductFormComponent } from './product-form/product-form.component'
 import { ProductListingComponent } from './product-listing/product-listing.component'
@@ -12,10 +13,12 @@ const routes: Routes = [
 	{
 		path: 'create',
 		component: ProductFormComponent,
+		canActivate: [AuthGuard],
 	},
 	{
 		path: 'edit/:id',
 		component: ProductFormComponent,
+		canActivate: [AuthGuard],
 	},
 ]
 
