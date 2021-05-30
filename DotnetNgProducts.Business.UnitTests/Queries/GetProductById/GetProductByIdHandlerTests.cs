681b06ebec31775fcbda14e521d9e66654465ec7
Luis Henrique Paim <luis.paim@poatek.com>
2021-05-30 18:29:46 -0300

    Add unit tests for GetProductByIdHandler

---
+++ DotnetNgProducts.Business.UnitTests/Queries/GetProductById/GetProductByIdHandlerTests.cs
@@ -0,0 +1,46 @@
+﻿using DotnetNgProducts.Business.Queries;
+using DotnetNgProducts.Data;
+using DotnetNgProducts.Models;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DotnetNgProducts.Business.UnitTests.Queries.GetProductById
+{
+    [TestClass]
+    public class GetProductByIdHandlerTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException), "System.ArgumentNullException: Value cannot be null. (Parameter 'request')")]
+        public async Task HandlingGetProductByIdShouldThrowArgumentNullException()
+        {
+            var handler = new GetProductByIdHandler(default);
+
+            // Act
+            await handler.Handle(default, CancellationToken.None);
+        }
+
+        [TestMethod]
+        public async Task HandlingSaveProductShouldReturnSaveProductResponse()
+        {
+            // Arrange
+            var product = new Product { Id = 1, Name = "Name", Price = 0.5m };
+            var repository = Substitute.For<IProductRepository>();
+            _ = repository
+                .GetAsync(product.Id)
+                .Returns(product);
+
+            var handler = new GetProductByIdHandler(repository);
+
+            // Act
+            var response = await handler.Handle(new GetProductByIdRequest(product.Id), CancellationToken.None);
+
+            // Assert
+            _ = response.Should().BeOfType<Product>();
+            _ = response.Name.Should().Be("Name");
+        }
+    }
+}
