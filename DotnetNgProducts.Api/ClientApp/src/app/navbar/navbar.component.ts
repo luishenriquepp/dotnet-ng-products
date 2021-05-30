2aef023e1a9191e9779ec4a7e029765ae8d0a9bf
Luis Henrique Paim <luis.paim@poatek.com>
2021-05-30 20:16:35 -0300

    Add login event to observe

--- DotnetNgProducts.Api/ClientApp/src/app/navbar/navbar.component.ts
+++ DotnetNgProducts.Api/ClientApp/src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core'
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core'
 import { LocalStorageService } from '../services/local-storage.service'
 
 @Component({
@@ -7,8 +7,14 @@ import { LocalStorageService } from '../services/local-storage.service'
 	styleUrls: ['./navbar.component.css'],
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class NavbarComponent {
-	constructor(private localStorageService: LocalStorageService) {}
+export class NavbarComponent implements OnInit {
+	constructor(private localStorageService: LocalStorageService, private changeDetector: ChangeDetectorRef) {}
+
+	ngOnInit(): void {
+		this.localStorageService.getLoginStatus().subscribe((login) => {
+			this.changeDetector.detectChanges()
+		})
+	}
 
 	get isLogged(): boolean {
 		return this.localStorageService.isLogged()
--- DotnetNgProducts.Api/ClientApp/src/app/services/local-storage.service.ts
+++ DotnetNgProducts.Api/ClientApp/src/app/services/local-storage.service.ts
@@ -1,20 +1,28 @@
 import { Injectable } from '@angular/core'
+import { BehaviorSubject, Observable } from 'rxjs'
 
 @Injectable({
 	providedIn: 'root',
 })
 export class LocalStorageService {
 	private authKey = 'auth-token'
+	private onLogin = new BehaviorSubject<boolean>(undefined)
 
 	public addAuth(value: string): void {
 		localStorage.setItem(this.authKey, value)
+		this.onLogin.next(true)
 	}
 
 	public removeAuth(): void {
 		localStorage.removeItem(this.authKey)
+		this.onLogin.next(false)
 	}
 
 	public isLogged(): boolean {
 		return !!localStorage.getItem(this.authKey)
+	}
+
+	public getLoginStatus(): Observable<boolean> {
+		return this.onLogin.asObservable()
 	}
 }
